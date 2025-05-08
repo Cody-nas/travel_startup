c5dcc858295c433bac4a1bb408850d63689dab75
Cody-nas <174928953+Cody-nas@users.noreply.github.com>
2025-05-08 06:15:21 +0100

    setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 
 const font = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -31,9 +32,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={` ${font.className} antialiased `}>
-        Nav
+        <ResponsiveNav />
         {children}
-        Footer
       </body>
     </html>
   );
