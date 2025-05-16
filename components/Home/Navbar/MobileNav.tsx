c780a3de295c8c62c68ff2b3b5510cd01a6097ae
Cody-nas <174928953+Cody-nas@users.noreply.github.com>
2025-05-16 10:44:56 +0100

    feat: enhance MobileNav with overlay and dynamic nav links; update Nav component for fixed positioning

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -1,7 +1,27 @@
+import { navLinks } from "@/constant/constant";
+import Link from "next/link";
 import React from "react";
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <div>
+      {/* overlay */}
+      <div className="fixed insert-0 transform transition-all duration-500 z-[1002] bg-black opacity-70 w-full h-screen"></div>
+
+      {/* navlinks */}
+      <div className="textwhite fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[60%] bg-rose-900 space-y-6 z-[1050]">
+        {navLinks.map((link) => {
+          return (
+            <Link href={link.url} key={link.id}>
+              <p className="text-white w-fit text-[20px] ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]">
+                {link.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default MobileNav;
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -7,7 +7,7 @@ import { ImAirplane } from "react-icons/im";
 
 const Nav = () => {
   return (
-    <div className="bg-blue-950 transition-all duration-200 h-[12vh] z-[1000]">
+    <div className="bg-blue-950 transition-all duration-200 h-[12vh] z-[1000] fixed w-full">
       <div className="flex items-center h-full justify-between w-[90%]  xl:w-[80%] mx-auto">
         {/* logo */}
         <div className="flex items-center space-x-2">
