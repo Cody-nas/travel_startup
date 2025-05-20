37c0107fde997eae8a86ace5fb213225f27d216e
Cody-nas <174928953+Cody-nas@users.noreply.github.com>
2025-05-21 00:46:42 +0100

    feat: implement Hero component with video background and overlay text

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="relative w-full h-[120vh] sm:h-[100vh]">
+      <div className="absolute top-0 left-0 w-full h-full bg-gray-800 opacity-70"></div>
+      <video
+        src="/images/hero1.mp4"
+        autoPlay
+        muted
+        loop
+        preload="metadata"
+        className="w-full h-full object-cover"
+      />
+      {/* text content */}
+      <div className="absolute z-[100] w-full h-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[50%]">
+        <div className="flex items-center justify-center flex-col w-full h-full">
+          <div>
+            <h1 className="text-[25px] mb-4 md:mb-0 text-center md:text-[35px] lg:text-[45px] tracking-[0.7rem] text-white font-bold ">
+              LETS ENJOY THE NATURE
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
