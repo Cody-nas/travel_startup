ff9888bc979f68764c410de1edce2f0b06d254aa
Cody-nas <174928953+Cody-nas@users.noreply.github.com>
2025-05-23 09:46:23 +0100

    feat: add video background and overlay text to Hero component

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
         <div className="flex items-center justify-center flex-col w-full h-full">
           <div>
             <h1 className="text-[25px] mb-4 md:mb-0 text-center md:text-[35px] lg:text-[45px] tracking-[0.7rem] text-white font-bold ">
-              LETS ENJOY THE NATURE
+              lets enjoy the nature
             </h1>
           </div>
         </div>
